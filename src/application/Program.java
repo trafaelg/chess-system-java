ad60e97969d4d3f359711a24b1b995a0159ef827
trafaelg <trafaelg@everis.com>
2021-03-31 15:49:49 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,31 +25,30 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
-				boolean [][] possibleMoves = chessMatch.possibleMoves(source);
-	 			UI.clearScreen();
+
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
 			}
-			
+
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}   
-			
+			}
+
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				sc.nextLine(); 
+				sc.nextLine();
 			}
 		}
 	}
-} 
+}
